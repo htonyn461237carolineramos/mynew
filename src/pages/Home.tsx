6a51c13c5fbc7dc145ea207f8d49210af5e21f99
余煌伟 <yuhuangwei@bonree.com>
2025-11-04 15:22:46 +0800

    feat: add home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+/**
+ * home
+ */
+
+type HomeProps = {
+  //
+};
+
+const Home = (props: HomeProps) => {
+  console.log("Home props:", props);
+  return <div>Home</div>;
+};
+
+export default Home;
